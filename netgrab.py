1d6cd44c59fb5ce2e310ccb3c57c680aec287d45
Silver Sun <itisagift@yeah.net>
2016-07-14 18:37:08 +0800

    get donw to the net modules

---
+++ netgrab.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding : utf-8 -*-
+
+import requests, codecs;
+
+wget_es = {
+    0: "No problems occurred.",
+    2: "User interference.",
+    1<<8: "Generic error code.",
+    2<<8: "Parse error - for instance, when parsing command-line " \
+        "optio.wgetrc or .netrc...",
+    3<<8: "File I/O error.",
+    4<<8: "Network failure.",
+    5<<8: "SSL verification failure.",
+    6<<8: "Username/password authentication failure.",
+    7<<8: "Protocol errors.",
+    8<<8: "Server issued an error response."
+};
+
+headers = {
+    "Accept":"text/html,application/xhtml+xml,application/xml; " \
+        "q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Encoding":"text/html",
+    "Accept-Language":"en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4,zh-TW;q=0.2",
+    "Content-Type":"application/x-www-form-urlencoded",
+    "User-Agent":"Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 " \
+        "(KHTML, like Gecko) Chrome/32.0.1700.77 Safari/537.36"
+};
